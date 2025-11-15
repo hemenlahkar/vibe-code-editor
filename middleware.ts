8c9213c8fff73ae28bd5ab06ea279d7d40dd11f7
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2025-11-16 00:51:57 +0530

    add authentication setup

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import { apiAuthPrefix, DEFAULT_LOGIN_REDIRECT, authRoutes, publicRoutes } from "./routes";
+import authConfig from "./auth.config";
+
+const {auth} = NextAuth(authConfig)
+
+export default auth((req)=>{
+    const {nextUrl} = req;
+    const isLoggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isApiAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if(!isLoggedIn && !publicRoutes)
+    {
+        return Response.redirect(new URL("/auth/sign-in", nextUrl));
+    }
+
+});
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
+};
